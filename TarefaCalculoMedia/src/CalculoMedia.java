10ada7e3ed82c96345908326ce95d65be363d470
Leonardo <leonardo@aulas.com>
2025-10-06 20:27:52 -0300

    Correção da atividade

--- TarefaCalculoMedia/src/CalculoMedia.java
+++ TarefaCalculoMedia/src/CalculoMedia.java
@@ -1,8 +1,12 @@
 public class CalculoMedia {
+    public static void main(String[] args) {
     int notaA = 10;
     int notaB = 7;
     int notaC = 5;
     int notaD = 8;
 
-    int media = (notaA + notaB + notaC + notaD)/2;
+    int media = (notaA + notaB + notaC + notaD)/4;
+
+        System.out.println(media);
+    }
 }
